47607193bc534d3a1890ec7d1675ca41e3fa0207
AWS ParallelCluster user <ec2-user@ip-10-0-3-118.ap-southeast-1.compute.internal>
2021-11-08 15:24:37 +0000

    added in python time command

--- fsall-slurm.sh
+++ fsall-slurm.sh
@@ -13,10 +13,12 @@
 #SBATCH -e fsall-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python -u -c "import time; \
 import DataProcessingTools as DPT; \
 lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
 lfall.save(); \
 hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
 hfall.save();
+print(time.localtime());"
 
 aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:514297294947:awsnotify --message "FSJobDone"import DataProcessingTools as DPT; \
