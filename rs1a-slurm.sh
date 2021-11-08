03198bc283457848072916e6431c99f7c9ef1292
Yang Jing <65772285+YJ048@users.noreply.github.com>
2021-11-08 21:24:42 +0800

    Update rs1a-slurm.sh

--- rs1a-slurm.sh
+++ rs1a-slurm.sh
@@ -19,19 +19,8 @@ import os; \
 import time; \
 t0 = time.time(); \
 print(time.localtime()); \
-<<<<<<< HEAD
-DPT.objects.processDirs(dirs=None, objtype=pyh.RPLSplit, channel=[*range(1,33)], SkipHPC=False, HPCScriptsDir = '/data/src/PyHipp/', SkipLFP=False, SkipHighPass=False, SkipSort=False);\
-DPT.objects.processDirs(dirs=['sessioneye/array01','session01/array01'], cmd='import PyHipp as pyh; import DataProcessingTools as DPT; DPT.objects.processDirs(None, pyh.RPLLFP, saveLevel=1); DPT.objects.processDirs(None, pyh.RPLHighPass, saveLevel=1);'); \
-os.chdir('session01/array01'); \
-DPT.objects.processDirs(level='channel', cmd='import PyHipp as pyh; from PyHipp import mountain_batch; mountain_batch.mountain_batch(); from PyHipp import export_mountain_cells; export_mountain_cells.export_mountain_cells();'); \
-print(time.localtime()); \
-print(time.time()-t0);"
-
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:514297294947:awsnotify --message "RPLS1aJobDone"
-=======
 DPT.objects.processDirs(dirs=None, objtype=pyh.RPLSplit, channel=[*range(1,33)], SkipHPC=False, HPCScriptsDir='/data/src/PyHipp/', SkipLFP=False, SkipHighPass=False, SkipSort=False); \
 print(time.localtime()); \
 print(time.time()-t0);"
 
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:018084650241:awsnotify --message "RPLS1JobDone"
->>>>>>> upstream/main
+aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:514297294947:awsnotify --message "RPLS1aJobDone"
